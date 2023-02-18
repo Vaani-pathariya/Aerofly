ec86988b0c6f164cea1cc894898ec6aad88600a6
Vaani <vaanipathariya@gmail.com>
2023-02-18 16:28:50 +0530

    adding backend functionality

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose=require('mongoose');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+mongoose.connect('',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
